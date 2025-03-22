3299b4e78e3878fd9e1b028d120e32b3cd5c7874
RodriguesClaudiane <claudianerodrigues365@gmail.com>
2025-03-22 11:01:47 -0300

    feat:criando o app

---
+++ todo-app/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>faz de conta que... </h1>
+      <h2>eu</h2>
+      <h3>sei</h3>
+      <h4>programar</h4>
+    </>
+  )
+}
+
+export default App
---
+++ todo-app/src/components/Card/CardTask.tsx
@@ -0,0 +1,7 @@
+export default function CardTask(){
+    return(
+        <>
+        <h1></h1>
+        </>
+    )
+}
